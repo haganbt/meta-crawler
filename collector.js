3eb2f8bd7a3f8cc81c03d27179027c805ad86e0d
Ben Hagan <benhagan@gmail.com>
2014-07-21 09:05:47 -0700

    Latest.

--- collector.js
+++ collector.js
@@ -1,5 +1,5 @@
 var Crawler = require("simplecrawler");
-var crawler = Crawler.crawl("http://hp.com");
+var crawler = Crawler.crawl("http://ford.com/");
 var moment  = require('moment');
 var store   = {};
 var fs      = require('fs');
@@ -8,7 +8,7 @@ crawler.interval = 500;
 crawler.maxConcurrency = 3;
 crawler.timeout = 5000;
 crawler.stripQuerystring = true;
-//crawler.initialPath = '/us/en/';
+//crawler.initialPath = '/us/en_us/';
 
 
 crawler.addFetchCondition(function(parsedURL) {
@@ -17,24 +17,53 @@ crawler.addFetchCondition(function(parsedURL) {
         //console.log("INFO: Ignoring ",parsedURL.path);
         return false;
     }
-
     return true;
 });
 
 
 crawler.on("fetchcomplete",function(queueItem, responseBuffer, response) {
 
+    var path =  _cleanPath(queueItem.path);
+    console.log('RECEIVED: ' + queueItem.path);
+
     try {
 
-        console.log('RECEIVED: ' + queueItem.path);
         var result = responseBuffer.toString('utf-8')
+        var title = result.match(/<meta(?=[^>]*\bname="title")[^>]*\bcontent="(.*?)".*/i)[1];
+        //var description = result.match(/<meta(?=[^>]*\bname="description")[^>]*\bcontent="(.*?)".*/i)[1];
+
+        if(title !== null && title !== '' && path !== null){
+            console.log(moment().format('MMMM Do YYYY, h:mm:ss a') + ' - Processing TITLE response.');
+            store[path] = store[path] || {};
+
+            title = title.replace(/^\s+|\s+$/g, '');  // trim
+            var allTitles = title.split('.');
+
+            for (var y = 0; y < allTitles.length; y++) {
+
+                allTitles[y] = allTitles[y].replace(/^\s+|\s+$/g, '');  // trim
+
+                if(allTitles[y] === ''){
+                    continue;
+                }
+
+                store[path][allTitles[y]] = store[path][allTitles[y]] || 0;
+                store[path][allTitles[y]] ++;
+        }
+
+        }
+
+    } catch (e){
+        console.log('ERROR: No TITLE data - ' + e);
+    }
+
+
+    // keywords
+    try {
+
+        var result = responseBuffer.toString('utf-8')
+
         var keywords = result.match(/<meta(?=[^>]*\bname="keywords")[^>]*\bcontent="(.*?)".*/i)[1];
-        //var description = result.match('<meta name="Description" content="(.*?)">')[1];
-        var path =  _cleanPath(queueItem.path);
-
-
-
-
 
         if(keywords !== null && keywords !== '' && path !== null){
             console.log(moment().format('MMMM Do YYYY, h:mm:ss a') + ' - Processing response.');
@@ -55,18 +84,17 @@ crawler.on("fetchcomplete",function(queueItem, responseBuffer, response) {
                 store[path][allKeywords[i]] ++;
             }
 
-            // save
-            if(~~(Math.random() * 10) + 1 === 3){
-                _save(store);
-            }
-
-
         }
 
     } catch (e){
-        console.log('ERROR: No matching meta data - ' + e);
+        console.log('ERROR: No KEYWORD data - ' + e);
     }
 
+
+    // save
+    if(~~(Math.random() * 10) + 1 === 3){
+        _save(store);
+    }
 
 });
 
@@ -95,7 +123,7 @@ function _endsWith(str, suffix) {
 }
 
 function _save(data) {
-    fs.writeFile("hp.json", JSON.stringify(data), function(err) {
+    fs.writeFile("ford.json", JSON.stringify(data), function(err) {
         if(err) {
             console.log(err);
         } else {
--- reader-simple.js
+++ reader-simple.js
@@ -1,7 +1,7 @@
 fs = require('fs')
 
 
-fs.readFile('data.txt', 'utf8', function (err,data) {
+fs.readFile('ford.json', 'utf8', function (err,data) {
 
     if (err) {
         return console.log(err);
@@ -12,11 +12,11 @@ fs.readFile('data.txt', 'utf8', function (err,data) {
     for(var urlInd in all) {
         if(all.hasOwnProperty(urlInd)){
 
-            console.log(urlInd);
+           // console.log('URL:" '+urlInd);
             //console.log(all[urlInd]);
 
             for(var key in all[urlInd]) {
-                console.log('  -- ' + key + ': ' + all[urlInd][key]);
+                console.log(key);
             }
 
         }
